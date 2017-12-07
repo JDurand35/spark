d5431da488d56c21bc15c6cc0b9eb37909b3a4ab
bynaryman <i.f.lledoux@gmail.com>
2017-12-07 20:27:49 +0100

    improve integral.py : stats workers/rect

--- Integral.py
+++ Integral.py
@@ -13,12 +13,11 @@ from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
 
-    spark = SparkSession\
-    .builder\
-    .appName("PythonIntegral")\
-    .getOrCreate()
-
+    
     partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+
+    # The path for school VM
+    PATH = '/home/louis/spark/outputIntegral.csv'
 
     a = 1
     b = 10
@@ -29,6 +28,7 @@ if __name__ == "__main__":
     sum = 0
     values = []
     time_values = []
+    temp_time_values = []
     meantime = 0
 
     values.append('val')
@@ -36,23 +36,41 @@ if __name__ == "__main__":
     def integrale(index):
         return f(a+index*p)
 
-    for i in [1,10,50,100,1000,10000,100000]:
+    for i in [10**q for q in range(5)]:
+        
+        print('nb of rect', i)
+
         def integrale(index):
             return f(a+index*(1.0*(b-a)/i))
-        for j in range(0, batch_size_for_mean):
-            starttime = time.time()
-            I = spark.sparkContext.parallelize(range(a, i+1), partitions).map(integrale).reduce(add)
-            stoptime = time.time()
-	    meantime = meantime + (stoptime - starttime)
-            sum = sum + I*(1.0*(b-a)/i)
+        
+        for workers in [2**k for k in range(6)]:
+            # print('    nb of workers', workers)
+            spark = SparkSession\
+                    .builder\
+                    .master("local[" + str(workers)  + "]")\
+                    .appName("PythonIntegral")\
+                    .getOrCreate()
+            
+            for j in range(0, batch_size_for_mean):
+                starttime = time.time()
+                I = spark.sparkContext.parallelize(range(a, i+1), partitions).map(integrale).reduce(add)
+                stoptime = time.time()
+                meantime = meantime + (stoptime - starttime)
+                sum = sum + I*(1.0*(b-a)/i)
+            print('    number of workers', workers, ' ::: Meantime (10 batch): ', meantime/10, 'Integral value: ', sum/10)
+            temp_time_values.append(str(workers) + ' workers')
+            temp_time_values.append(meantime/10)
+            sum = 0
+            meantime = 0
+            spark.stop()
+        time_values.append(str(i) + ' rectangles')
+        time_values.append(temp_time_values)
+        temp_time_values = []
 
-        values.append(sum/10)
-        time_values.append(meantime/10)
-        sum = 0
-        meantime = 0
-    spark.stop()
 
-    with open('/home/louis/spark/outputIntegral.csv', 'wb') as f:
+    print(time_values)
+
+    with open('outputIntegral.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(values)
         writer.writerow(time_values)
